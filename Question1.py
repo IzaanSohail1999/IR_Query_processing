008a36f160a01ac8d7a3408db80e82ae8cbe41b8
izaansohail <k180162@nu.edu.pk>
2021-03-29 01:05:13 +0500

    fifth commit

--- Question1.py
+++ Question1.py
@@ -69,11 +69,60 @@ def fileread(txtDir,Dir1):
                                 counter = counter + 1
 
 def search(term):
-    if term in Dict:
-        return Dict[term][0]
+    type = 0
+    value = []
+    term = term.split(' ')
+    for x in term:
+        if x == 'and':
+            print("boolean operator is : ", x)
+            type = 1
+        elif x == 'or':
+            print("boolean operator is : ", x)
+            type = 2
+        elif x == 'not':
+            print("boolean operator is : ", x)
+            type = 3
+        elif x in Dict:
+            value.append(Dict[x][0].keys())
+            
+    
+    if type == 0:
+        print(Dict[term][0])
+    if(type == 1):
+        print(set(value[0]).intersection(set(value[1])))
+        print(len(set(value[0]).intersection(set(value[1]))))
+    if(type == 2):
+        print(set(value[0]).union(set(value[1])))
+        print(len(set(value[0]).union(set(value[1]))))
+        
+    # for num in range(len(value[0])):
+    #     if value[0][num] == value[1][num]:
+    #         print(value[0][num])
+
+    # value[0].sort()
+    # value[1].sort()
+    # print(value[0])
+    # print(value[1])
+    # print(max(value[0]))
+    # print(max(value[1]))
+    # print(value[0][1])
+    # print(value[1][1])
+    # count = 0
+    # if type1 == 1:
+    # if len(value[0]) <= len(value[1]):
+    #         for num in range(len(value[0])):
+    #             if value[0][count] == value[1][count]:
+    #                 print(value[0][count])
+    #                 count = count + 1
+    # elif len(value[0]) > len(value[1]):
+    #         for num in range(len(value[1])):
+    #             if value[1][count] == value[0][count]:
+    #                 print(value[1][count])
+    #                 count = count + 1
+    # return Dict[term][0]
 
 def main():
-    Dict1 = {}
+    # Dict1 = {}
     global freqcounter
     Dir = "D:\\izaan\\Work\\University\\university docs\\Semester 6\\IR\\Assignment\\Assignment 1\\ShortStories\\"
     Dir1 = "D:\\izaan\\Work\\University\\university docs\\Semester 6\\IR\\Assignment\\Assignment 1\\"
@@ -82,15 +131,16 @@ def main():
     # print(Dict)
     # print(freqcounter)
     # query = input()
-    query = "old man"
+    query = "beard"
+    search(query)
     # print(query.split(' '))
-    query = query.split(' ')
+    # query = query.split(' ')
     # print(len(query))
-    for x in query:
-        if x == "and":
-            type = "1"
-            continue
-        else:
-            print(x)
-            Dict1 = search(x)
+    # for x in query:
+    #     if x == "and":
+    #         type = "1"
+    #         continue
+    #     else:
+    #         print(x)
+    #         search(x)
 main()
